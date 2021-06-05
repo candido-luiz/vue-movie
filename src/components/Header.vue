fa3ba942ccc551fb052928c578ee0cc1f19c559b
candido-idea-home <candidoluiz097@gmail.com>
2021-06-05 14:41:50 -0300

    feat: Cria componente Header

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+        <div class="home">HomeLink</div>
+        <input type="text" name="searchMovie" id="searchMovie" placeholder="buscar filme">
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style >
+    header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: darkslateblue;
+    color: #fff;
+    position: sticky;
+    top: 0;
+    padding: 10px;
+    z-index: 2;
+}
+.home{
+    font-size: 18px;
+}
+#searchMovie{
+    max-width: 400px;
+    width: 100%;
+    padding: 5px 7px;
+}
+
+</style>
