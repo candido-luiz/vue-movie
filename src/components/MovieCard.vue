253dfef9f3d058c97b81fd73b28722cebd89884e
candido-idea-home <candidoluiz097@gmail.com>
2021-06-05 15:51:54 -0300

    feat: Adiciona componentes MovieCard e MovieList à página inicial
    O componente MovieList é o responsável por exibir a lista de filmes na
    tela, enquanto o MovieCard é o componente que representa cada um dos
    filmes da lista de filmes

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="movieCard">
+        <div class="moviePoster">
+            <img :src="moviePosterPath" alt="poster">
+        </div>
+        <div class="movieInfo">
+            <div class="movieInfo-title">{{movie.title}}</div>
+            <div class="movieInfo-release">{{movieReleaseDate}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        movie: Object
+    },
+
+    computed:{
+        moviePosterPath: function(){
+            const POSTER_PATH_BASE = 'https://image.tmdb.org/t/p/w300';
+            let posterPath = this.movie.poster_path;
+
+            return POSTER_PATH_BASE + posterPath;
+        },
+
+        movieReleaseDate: function(){
+            let releaseDate = this.movie.release_date.slice(0,4);
+            return releaseDate;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <div class="movie-grid">
+        <div v-for="movie in moviesArray" :key="movie.id">
+            <MovieCard :movie="movie"/>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import fetchOptions from '../fetchOptions';
+import MovieCard from './MovieCard.vue'
+export default {
+    components:{
+        MovieCard
+    },
+
+    data(){
+        return{
+            moviesArray: []
+        }
+    },
+
+    created: async function(){
+        let fetchedMovies = await fetchOptions.getMovieList();
+        this.moviesArray = fetchedMovies;
+        console.log(this.moviesArray)
+    }
+}
+</script>
+
+<style>
+
+</style>
