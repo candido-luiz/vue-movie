e726a86283710e40e589ea25da490ab19880e27a
candido-idea-home <candidoluiz097@gmail.com>
2021-06-06 10:22:17 -0300

    feat: Confitura o vue-router e define as rotas utilizadas

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
+import MovieList from './components/MovieList';
+import MovieDetails from './components/MovieDetails';
+
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: MovieList
+    },
+
+    {
+      path: '/:id',
+      component: MovieDetails
+    }
+  ]
+});
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
