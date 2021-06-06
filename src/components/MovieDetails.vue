8489c66a3eaa9c5a66ff835e089ecf610902178f
candido-idea-home <candidoluiz097@gmail.com>
2021-06-06 10:11:41 -0300

    feat: Cria componente MovieDetails para exibir detalhes dos filmes

---
+++ src/components/MovieDetails.vue
@@ -0,0 +1,66 @@
+<template>
+    <main class="movieBackDrop" 
+    :style="{ backgroundImage: 'url('+ movieBackDropPath +')' }">
+        <div class="movieDetails">
+            <div class="movie-poster">
+                <img :src="moviePosterPath" :alt="movie.title">
+            </div>
+            <div class="movie-info">
+                <div class="info-title">{{ movie.title }}</div>
+                <div class="info-tagline">{{ movie.tagline }}</div>
+                <div class="info-overview">{{ movie.overview }}</div>
+                <div class="info-genres">{{ movieGenres }}</div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+import fetchOptions from '../fetchOptions';
+
+export default {
+    data(){
+        return{
+            movie: {}
+        }
+    },
+
+    computed: {
+        //concatena a url base de uma imagem com o path do backdrop, do objeto movie
+        movieBackDropPath: function(){
+            const BACKDROP_PATH_BASE = 'https://image.tmdb.org/t/p/w1280';
+            let backDropPath = this.movie.backdrop_path;
+            
+            return BACKDROP_PATH_BASE + backDropPath;
+        },
+
+        //Concatena url base de uma imagem com o path do poster, do objeto movie
+        moviePosterPath: function(){
+            const POSTER_PATH_BASE = 'https://image.tmdb.org/t/p/w300';
+            let posterPath = this.movie.poster_path;
+
+            return POSTER_PATH_BASE + posterPath;
+        },
+        // Retorna uma string com os gêneros do objeto movie
+        movieGenres: function(){
+            let genresArray = this.movie.genres;
+            let genresTypes = genresArray.map((genre) => {
+                return genre.name
+            });
+
+            return genresTypes.join(', ');
+        }
+    },
+
+    // Busca detalhes do filme pelo id, passado por parâmetro
+    created: async function(){
+        let idParam = this.$route.params.id;
+        let fetchedMovie = await fetchOptions.getMovieDetails(idParam);
+        this.movie = fetchedMovie;
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
