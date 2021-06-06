8e501c1aac278eacce2a225fbc560e74eaceb261
candido-idea-home <candidoluiz097@gmail.com>
2021-06-06 10:10:14 -0300

    feat: Adiciona método para buscar detalhes de filmes pela tmdb API

--- src/fetchOptions.js
+++ src/fetchOptions.js
@@ -1,14 +1,27 @@
 const URL_BASE = 'https://api.themoviedb.org/3/movie/popular?api_key='
 const API_KEY = 'd18230a62c17fab4dab7a444673ab6b0'
 
-const FETCH_URL = URL_BASE + API_KEY;
+const FETCH_MOVIELIST_URL = URL_BASE + API_KEY;
 
 export default {
     // Retorna um Array contendo os filmes Populares da tmdb API
     getMovieList: async function(){
-        let res = await fetch(FETCH_URL);
+        let res = await fetch(FETCH_MOVIELIST_URL);
         let json = await res.json();
         let movieList = json.results;
+        
         return movieList;
+    },
+
+    // Retorna um objeto com os detalhes de um filme, através de um id
+    getMovieDetails: async function(idParam){
+        const MOVIE_URL_BASE = 'https://api.themoviedb.org/3/movie/'
+        let fetchMovieUrl = `${MOVIE_URL_BASE}${idParam}?api_key=${API_KEY}`;
+
+        let res = await fetch(fetchMovieUrl);
+        let json = await res.json();
+
+        return json;
+        
     }
 } 
